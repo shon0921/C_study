7a63e95391b5ec27d8fe1d63a5eb06255883baa0
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-18 23:41:53 +0900

    Add files via upload

---
+++ Ch8/8-8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+	char ss[5] = "abcd";
+	char tt[5];
+	int i;
+
+	for(i=0;i<4; i++)
+	{
+		tt[i] = ss[3-i];
+	}
+	tt[4]='\0';
+
+	printf("거꾸로 출력한 결과==> %s \n",tt);
+}
