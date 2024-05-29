cf6f7718d216d419e9a1021540ae52333ea736ba
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-29 14:55:12 +0900

    Add files via upload

---
+++ Ch9/9-10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+
+	printf("a 값 입력 : ");
+	scanf("%d",&a);
+	printf("b 값 입력 : ");
+	scanf("%d",&b);
+
+	int *p1 = &a;
+	int *p2 = &b;
+
+	int tmp = *p1;
+	*p1=*p2;
+	*p2=tmp;
+
+	printf("바뀐 값 a는 %d, b는 %d",a,b);
+	
+}
